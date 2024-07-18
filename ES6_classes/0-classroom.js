3400649bc0608752b70fec5e68524b7f4ce994bd
William <will@penguin.com>
2024-07-18 19:08:01 -0400

    0

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,6 +1,13 @@
 // 0-classroom.js
+
+// Exporting the ClassRoom class as the default export
 export default class ClassRoom {
+  /**
+   * Creates an instance of ClassRoom.
+   * @param {number} maxStudentsSize - The maximum number of students in the class.
+   */
   constructor(maxStudentsSize) {
+    // Assigning the maxStudentsSize parameter to the _maxStudentsSize property
     this._maxStudentsSize = maxStudentsSize;
   }
 }
