159a91c7e7d95d3016744db597263c6b7f83d26d
William <will@penguin.com>
2024-07-18 20:55:09 -0400

    8

---
+++ ES6_classes/8-main.js
@@ -0,0 +1,6 @@
+import HolbertonClass from './8-holberton_class.js';
+
+const hc = new HolbertonClass(100, 'San Francisco');
+console.log(`The class size is ${Number(hc)}`);
+console.log(`The class location is ${String(hc)}`);
+
