8ac09fd24eee63739837c2e15fde09de3bf0b8d3
William <will@penguin.com>
2024-07-18 22:18:23 -0400

    6 lint fix

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -5,11 +5,9 @@ export default class SkyHighBuilding extends Building {
     super(sqft);
     this._floors = floors;
   }
-
   get floors() {
     return this._floors;
   }
-
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
