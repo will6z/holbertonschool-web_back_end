716314ba1a9daf40d5667a1b6c2eb65a3a059201
William <will@penguin.com>
2024-07-18 23:18:33 -0400

    6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,4 +1,4 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
@@ -6,11 +6,13 @@ export default class SkyHighBuilding extends Building {
     this._floors = floors;
   }
 
+  // Gett.. and sett.. for attribute
   get floors() {
     return this._floors;
   }
 
+  // Override the evacuationWa... method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
