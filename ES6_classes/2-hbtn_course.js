797fc360d76c9f9c1c03731c7c17c5757bd8146e
William <will@penguin.com>
2024-07-18 23:11:57 -0400

    2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,58 +1,56 @@
-// 2-hbtn_course.js
-
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateName(name);
-    this._length = this.validateLength(length);
-    this._students = this.validateStudents(students);
+    this._name = HolbertonCourse._validateName(name);
+    this._length = HolbertonCourse._validateLength(length);
+    this._students = HolbertonCourse._validateStudents(students);
   }
 
-  // Name getter and setter with validation
+  // Gett.. and sett for name
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    this._name = this.validateName(value);
+  set name(newName) {
+    this._name = HolbertonCourse._validateName(newName);
   }
 
-  // Length getter and setter with validation
+  // Gett.. and sett for lenght
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    this._length = this.validateLength(value);
+  set length(newLength) {
+    this._length = HolbertonCourse._validateLength(newLength);
   }
 
-  // Students getter and setter with validation
+  // Get.. and sett for student's
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    this._students = this.validateStudents(value);
+  set students(newStudents) {
+    this._students = HolbertonCourse._validateStudents(newStudents);
   }
 
-  // Validation methods
-  validateName(value) {
-    if (typeof value !== 'string') {
+  // Validation
+  static _validateName(name) {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    return value;
+    return name;
   }
 
-  validateLength(value) {
-    if (typeof value !== 'number') {
+  static _validateLength(length) {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    return value;
+    return length;
   }
 
-  validateStudents(value) {
-    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+  static _validateStudents(students) {
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    return value;
+    return students;
   }
 }
