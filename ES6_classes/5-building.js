2699ff60f0de1f6f2546566b80f5337483d14c3f
William <will@penguin.com>
2024-07-18 20:22:52 -0400

    5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,23 @@
-class Building {
+export default class Building {
+  // initialize the sqft attribute
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate an abstract class');
+    // (abstract class check)
+    if (new.target !== Building) {
+      this.evacuationWarningMessage();
     }
+    // Set the sqft attribute
     this._sqft = sqft;
   }
 
-  // Getter for sqft
+  // Gett.. for the sqft attribute
   get sqft() {
     return this._sqft;
   }
 
   // Abstract method
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
-
-export default Building;
