c11d144beafd8243e7c592a87129935563950f9f
William <will@penguin.com>
2024-07-23 14:06:43 -0400

    4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeInfo = newGrades.find(grade => grade.studentId === student.id);
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeInfo = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
         grade: gradeInfo ? gradeInfo.grade : 'N/A',
