144f71a954faabe76ffb930ccd19ae23ef6ae444
William <will@penguin.com>
2024-07-23 15:03:48 -0400

    10 fix

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -3,7 +3,7 @@ function updateUniqueItems(map) {
     throw new Error('Cannot process');
   }
 
-  for (let [key, value] of map) {
+  for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
     }
