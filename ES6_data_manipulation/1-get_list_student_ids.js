858bdb91bfaa31ecafc8646436a02f7d6dc706e7
William <will@penguin.com>
2024-07-23 13:47:30 -0400

    1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(list) {
   if (!Array.isArray(list)) {
     return [];
   }
-  return list.map(student => student.id);
+  return list.map((student) => student.id);
 }
