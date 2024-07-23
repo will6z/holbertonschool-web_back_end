05237e493ef2a8bd889dde1799876db9da1a39b4
William <will@penguin.com>
2024-07-23 14:08:02 -0400

    5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8View = new DataView(buffer);
+
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  int8View.setInt8(position, value);
+  return int8View;
+}
+
+export default createInt8TypedArray;
