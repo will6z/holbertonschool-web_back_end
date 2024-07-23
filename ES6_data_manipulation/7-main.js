28ad9eae44a3d24807cd9872c9fb1a3c1edf3294
William <will@penguin.com>
2024-07-23 14:49:03 -0400

    7

---
+++ ES6_data_manipulation/7-main.js
@@ -0,0 +1,5 @@
+import hasValuesFromArray from "./7-has_array_values.js";
+
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1])); // true
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10])); // false
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10])); // false
