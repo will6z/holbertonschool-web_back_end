a61dcd91e515c6bb0a3fd1acea805b91d2d055da
William <will@penguin.com>
2024-07-24 17:34:00 -0400

    6

---
+++ ES6_promise/6-final-user.test.js
@@ -0,0 +1,15 @@
+import handleProfileSignup from './6-final-user';
+
+test("handleProfileSignup returns the right array", async () => {
+  const queue = await handleProfileSignup('John', 'Doe', 'Gerald.jpg');
+  expect(queue).toEqual([
+    {
+      status: 'fulfilled',
+      value: { firstName: 'John', lastName: 'Doe' }
+    },
+    {
+      status: 'rejected',
+      value: new Error('Gerald.jpg cannot be processed')
+    },
+  ]);
+});
