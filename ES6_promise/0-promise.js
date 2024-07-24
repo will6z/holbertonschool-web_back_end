31f2dcaec71f8e3609bcc6fef37aad8e262ff9a3
William <will@penguin.com>
2024-07-24 17:06:38 -0400

    0 lint fix

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve("Response from API");
+  return new Promise((resolve) => {
+    resolve('Response from API');
   });
 }
 
