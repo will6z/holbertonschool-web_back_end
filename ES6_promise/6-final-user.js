eb41ad4b89c5eaa5edec62b5ea218eca8fbb5b1b
William <will@penguin.com>
2024-07-24 17:44:28 -0400

    6 fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,4 +1,4 @@
-ximport signUpUser from './4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
