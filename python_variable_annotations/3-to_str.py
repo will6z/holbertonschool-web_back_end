edaf865aa48cfb62514a6fb9cba6b82050514475
William <will@penguin.com>
2024-08-15 17:11:17 -0400

    3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function `to_str` that returns the string representation of a float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n (float): The number to convert to a string.
+
+    Returns:
+        str: The string representation of the number.
+    """
+    return str(n)
