5a70e7c58038a4f16af8903a6862ee3072c3db42
William <will@penguin.com>
2024-08-15 17:38:18 -0400

    1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
-""" spawns multiple `wait_random coroutines and returns their delays in ascending order """
-
+""" running many coroutines and returns delays"""
 import asyncio
 from typing import List
-from wait_random import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ returns a list """
-    delays = []
+    """returns a list of all delays"""
     tasks = [wait_random(max_delay) for _ in range(n)]
-    
-    for delay in await asyncio.gather(*tasks):
-        delays.append(delay)
+
+    delays = await asyncio.gather(*tasks)
+
+    # order without using sort
+    for i in range(len(delays)):
+        for j in range(0, len(delays) - i - 1):
+            if delays[j] > delays[j + 1]:
+                delays[j], delays[j + 1] = delays[j + 1], delays[j]
 
     return delays
