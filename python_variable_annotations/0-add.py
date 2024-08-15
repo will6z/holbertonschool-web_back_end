fc8a57977470789f3315639f4273adf9a2114d67
William <will@penguin.com>
2024-08-15 17:06:53 -0400

    0 fix again

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,11 +5,9 @@ This module provides a function `add` that adds two floats and returns the sum.
 def add(a: float, b: float) -> float:
     """
     Adds two float numbers and returns the sum as a float.
-
     Args:
         a (float): The first number.
         b (float): The second number.
-
     Returns:
         float: The sum of the two numbers.
     """
