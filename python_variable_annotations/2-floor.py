ed2e64cc231497a4a0a058f7f4cfb132fcb5f10e
William <will@penguin.com>
2024-08-15 17:09:34 -0400

    2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides a function `floor` that returns the floor of a float.
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        int: The floored value of the number.
+    """
+    return math.floor(n)
