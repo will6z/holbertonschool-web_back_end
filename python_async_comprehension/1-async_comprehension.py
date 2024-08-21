339d0cb187b6a22fdd0dc5cebf5dfc83a22e87de
William <will@penguin.com>
2024-08-21 18:28:22 -0400

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
-"""async comprehension task 1"""
+"""measure runtime for parallel execution"""
 import asyncio
+import time
 from typing import List
-from . import async_generator
 
-async def async_comprehension() -> List[float]:
-    """Collects 10 random numbers from async_generator using async comprehension"""
-    return [i async for i in async_generator()]
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+""" run async_comprehension four times in parallel and measure time taken """
+    start_time = time.time()
+    
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    
+    end_time = time.time()
+
+    return end_time - start_time
