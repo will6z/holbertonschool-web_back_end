f51aa3ef1f2dc3ca29dd2494053d804d869f0ff6
William <will@penguin.com>
2024-08-25 14:22:26 -0400

    1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,38 +1,49 @@
-import csv
-from typing import List
 
-def index_range(page: int, page_size: int) -> tuple:
-    """ calculate the start and end indexes for a page of data """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+#!/usr/bin/env python3
+"""simple func for pagination"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    func returns a tuple containing the start
+    and the end indexes
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
 
 class Server:
-    """server class to paginate a database of popular baby names"""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]  # Skip the header
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ return a page of data from the datase of baby name """
-        assert isinstance(page, int) and page > 0, "page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
+        """method retrieves page from dataset"""
+        assert isinstance(page, int) and page > 0, \
+            "Page have to be positive integer."
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be a positive number."
 
-        start_index, end_index = index_range(page, page_size)
+        start, end = index_range(page, page_size)
+
         dataset = self.dataset()
 
-        if start_index >= len(dataset):
-            return []
-
-        return dataset[start_index:end_index]
-
+        return dataset[start:end]
