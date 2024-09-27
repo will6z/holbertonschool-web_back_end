c0f176d9fd8a6c5354ba449c6c2009bf1b02d548
William <will@penguin.com>
2024-09-27 14:27:24 -0400

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,17 @@
-// 1-stdin.js
+const readline = require('readline');
 
-console.log("Welcome to Holberton School, what is your name?");
-
-process.stdin.on('data', (data) => {
-  // Normalize the input by removing both \r and \n characters
-  const name = data.toString().trim().replace(/\r?\n|\r/g, '');
-
-  // Output the name
-  console.log(`Your name is: ${name}`);
-
-  // Exit the process after the output
-  process.exit();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-process.on('exit', () => {
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+rl.on('close', () => {
   console.log('This important software is now closing');
 });
-
