2f8603a6497fe6ede55cdc918e552a998d9ec9d8
William <will@penguin.com>
2024-11-14 21:02:22 -0400

    100

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -1,14 +1,25 @@
 import Car from './10-car.js';
 
+const cloneSymbol = Symbol('clone');
+
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  // Override cloneCar to return an instance of Car instead of EVCar
+  // Override cloneCar to return a Car instance instead of EVCar
+  [cloneSymbol]() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+
+  // Public method to call the symbol method
   cloneCar() {
-    const clonedCar = new Car(this._brand, this._motor, this._color);
-    return clonedCar;
+    return this[cloneSymbol]();
+  }
+
+  // Getter for the species
+  get species() {
+    return 'EVCar';
   }
 }
