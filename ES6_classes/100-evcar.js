d72dfcac8522eeaab1a76bb96101b8689a997dc5
William <will@penguin.com>
2024-11-14 21:01:21 -0400

    199

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -2,17 +2,13 @@ import Car from './10-car.js';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    // Call the parent class's constructor to initialize brand, motor, and color
     super(brand, motor, color);
-    this._range = range; // New property for electric vehicle range
+    this._range = range;
   }
 
-  // Override cloneCar to return a Car instance, not EVCar
+  // Override cloneCar to return an instance of Car instead of EVCar
   cloneCar() {
-    const clonedCar = super.cloneCar(); // Use the Car's clone method
-    // Set the range to undefined since it's not part of Car class
-    clonedCar._range = undefined;
+    const clonedCar = new Car(this._brand, this._motor, this._color);
     return clonedCar;
   }
 }
-
